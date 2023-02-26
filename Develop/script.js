0e85f06c9b23bc19190db4cee1c133f412c0e122
snerowski <118015679+snerowski@users.noreply.github.com>
2023-02-26 17:42:00 -0500

    fixing deployment issue

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var confirmCharacter;
 var confirmUppercase; 
 var confirmLowercase;
 var choices;
-
+// created array with all the different characters
 var number = ["0","1","2","3","4,","5","6","7","8","9"];
 
 var character = ["!","@","#","$","%","^","&","*","(",")","_","+","=","-","/","~","`","<",">","?",".",";",":"];
@@ -15,7 +15,8 @@ var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
 var alpha2 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 
-
+// created generate password funtion that asked user to select character amount between 8 and 128 characters as per instructions
+// as well as else if statements that forces user to pick at least one parameter,   
 function generatePassword() {
   
   enter = prompt("Select character amount between 8 and 128 characters");
@@ -90,7 +91,7 @@ function generatePassword() {
   
 
   var password = [];
-
+// created for loop that randomizes character combinations for user
   for (var i = 0; i < enter; i++){
     var pickChoices = choices[Math.floor(Math.random()* choices.length)];
     password.push(pickChoices);
@@ -107,10 +108,11 @@ function generatePassword() {
 }
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+// added .join function to remove commas between characters
   passwordText.value = (password.join(''));
   console.log(password);
   
